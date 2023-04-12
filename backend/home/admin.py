7b8ff4b75894a9a2bf643036f6251630e6ad363b
crowdbotics-dev <engineering@crowdbotics.com>
2023-04-12 12:28:17 +0000

    Update models

--- backend/home/admin.py
+++ backend/home/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from .models import Testing
+admin.site.register(Testing)
 
 # Register your models here.
--- backend/home/models.py
+++ backend/home/models.py
@@ -1,1 +1,5 @@
+from django.conf import settings
 from django.db import models
+class Testing(models.Model):
+    'Generated Model'
+    testui = models.BigIntegerField()
