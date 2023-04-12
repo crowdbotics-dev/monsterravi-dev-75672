12845c4cbbeff1db12d29e4c530fafe29624774d
Crowdbotics <engineering@crowdbotics.com>
2023-04-12 12:23:56 +0000

    Update store.

--- store/newConnectorravi/api.js
+++ store/newConnectorravi/api.js
@@ -1,14 +1,7 @@
 import axios from "axios"
-import {
-  NEW_CONNECTOR_RAVI_USERNAME,
-  NEW_CONNECTOR_RAVI_PASSWORD
-} from "react-native-dotenv"
+import {} from "react-native-dotenv"
 const newConnectorravi = axios.create({
   baseURL: "https://google.com",
-  auth: {
-    username: NEW_CONNECTOR_RAVI_USERNAME,
-    password: NEW_CONNECTOR_RAVI_PASSWORD
-  },
   headers: { Accept: "application/json", "Content-Type": "application/json" }
 })
 export const apiService = {}
